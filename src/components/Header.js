3030024bcc491abb0411298b3549e8575bbe48db
John Christopher Dee <johndee0813@gmail.com>
2022-10-20 17:38:46 +0800

    Aborting this crash course. Will be learning from Web Dev Simplified instead

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Button from "./Button";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Task Tracker</h1>
+      <Button text="Add" />
+    </header>
+  );
+};
+
+export default Header;
